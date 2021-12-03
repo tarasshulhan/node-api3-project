70271531a8cd12d94f3de3054ece9d4bfec237a3
Taras Shulhan <taras.shulhan@gmail.com>
2021-12-03 16:39:28 +0200

    some tests passing

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,58 @@
+const Users = require('../users/users-model')
+const Posts = require('../posts/posts-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try{
+    const user = await Users.getById(req.params.id);
+    if(user){
+      req.user = user;
+      next();
+    }else{
+      next({ status: 404, message: "user not found" })
+    }
+  }catch(error){
+    next(error);
+}
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+    if (!req.body.name || !req.body.name.trim()) {
+      next({ status: 400, message: "missing required name field" });
+    } else {
+      next();
+    }
+  
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  if (!req.body.text || !req.body.text.trim()) {
+    next({ status: 400, message: "missing required text field" });
+  } else {
+    next();
+  }
 }
 
-// do not forget to expose these functions to other modules
+function errorHandling(err, req, res, next) { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+  errorHandling
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,22 +1,40 @@
 const express = require('express');
-
+const {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+  errorHandling,
+} = require('../middleware/middleware')
+const Users = require('../users/users-model')
+const Posts = require('../posts/posts-model');
+const { json } = require('express');
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  Users.get()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(error => {
+      next(error)
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res, next) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
+  res.status(200).json(req.user)
 });
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
+
 });
 
 router.put('/:id', (req, res) => {
@@ -41,4 +59,6 @@ router.post('/:id/posts', (req, res) => {
   // and another middleware to check that the request body is valid
 });
 
-// do not forget to export the router
+router.use(errorHandling);
+
+module.exports = router;
